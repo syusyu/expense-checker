773c30c4ea7f8840f51aef4fd8c557a5dbabf056
Hisamitsu Okabe <okabe@worksap.co.jp>
2018-08-30 15:59:04 +0900

    feat: wrote test of Expense

--- test/Expense.test.js
+++ test/Expense.test.js
@@ -4,6 +4,7 @@ import  { configure, shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 import Expense from '../src/Expense';
+import {mount} from "enzyme/build/index";
 
 configure({ adapter: new Adapter() });
 
@@ -13,67 +14,16 @@ describe('<Expense />', () => {
     it('Show expenditure', () => {
         const expense = {
             records: [
-                {date: '2018/08', expenditure: 1000, color: '#EEEEEE'},
-                {date: '2018/07', expenditure: 2000, color: '#EEEEEE'},
+                {date: '2018-08-01', expenditure: 1000, color: '#EEEEEE'},
+                {date: '2018-07-01', expenditure: 2000, color: '#EEEEEE'},
             ],
             sum: 3000,
         };
 
-        const wrapper = shallow(<Expense expense={expense} classes={dummyClasses} />);
-        // console.log(`html=${wrapper.html()}`);
+        const wrapper = mount(<Expense expense={expense} classes={dummyClasses} />);
+        const months = wrapper.find('span').map(node => node.text());
+        const expenditures = wrapper.find('h1').map(node => node.text());
+        expect(months).toEqual(['2018/08', '2018/07', 'Total']);
+        expect(expenditures).toEqual(['JP¥ 1,000', 'JP¥ 2,000', 'JP¥ 3,000', ]);
     });
 });
-
-// describe('<Expense />', () => {
-//     it('Existing child component (Expense)', () => {
-//         const wrapper = shallow(
-//             <Expense updateFiles={null} classes={{cardExpenditure: "foo"}}
-//                                          expense={{sum:0, warnings:[], records:[{date:'2018/08', expenditure:3000, color:'red'}]}} />
-//         );
-//         // const tree = renderer.create(
-//         //     <Expense updateFiles={null} classes={{cardExpenditure: "foo"}}
-//         //              expense={{sum:0, warnings:[], records:[{date:'2018/08', expenditure:3000, color:'red'}]}} />
-//         // ).toJSON();
-//         console.log(`###div=${wrapper.find('div').childAt(0)}`)
-//         expect(wrapper.find('div').childAt(0)).toBe(<div/>);
-//     });
-// });
-
-// describe('<App />', () => {
-    // it('this.state.textを更新した時にclass名に反映されること', () => {
-    //     const wrapper = shallow(<App />);
-    //     wrapper.setState({
-    //         text: 'AAA',
-    //     });
-    //     expect(wrapper.find('.AAA').length).toBe(1);
-    // });
-    //
-    // it('handleChangeを呼び出すと、setStateが呼び出されること', () => {
-    //     const wrapper = shallow(<App />);
-    //     const setStateSpy = jest.spyOn(App.prototype, 'setState');
-    //     wrapper.instance().handleChange('BBB');
-    //     expect(setStateSpy).toHaveBeenCalledWith({
-    //         inputValue: 'BBB',
-    //     });
-    // });
-    //
-    // it('handleClickを呼び出すと、setStateが呼び出されること', () => {
-    //     const wrapper = shallow(<App />);
-    //     const setStateSpy = jest.spyOn(App.prototype, 'setState');
-    //     wrapper.setState({
-    //         inputValue: 'CCC',
-    //     });
-    //     wrapper.instance().handleClick();
-    //     expect(setStateSpy).toHaveBeenCalledWith({
-    //         text: 'CCC',
-    //         inputValue: '',
-    //     });
-    // });
-    //
-    // test('<App />のスナップショット', () => {
-    //     const tree = renderer
-    //         .create(<App />)
-    //         .toJSON();
-    //     expect(tree).toMatchSnapshot();
-    // });
-// });
